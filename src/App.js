ee1a5c6968bbade7b6b59198e1bf2a62e6b156bc
rohail321 <qrohail32@gmail.com>
2023-07-25 04:10:39 +0100

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+
+import "./server";
+
 import Home from "./components/Pages/Home/Home";
 import About from "./components/Pages/About/About";
 import Header from "./components/layout/header/Header";
+import Vans from "./components/Pages/Van/Vans";
+import VanDetail from "./components/Pages/Van/VanDetail";
 
 function App() {
   return (
@@ -11,6 +16,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
+        <Route path='/van/:id' element={<VanDetail />} />
       </Routes>
     </div>
   );
